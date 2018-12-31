4688b5a45907e339c8babb9d6deea75764d7779d
fabioricali <ricalifabio@gmail.com>
2018-12-31 12:53:29 +0100

    progress

--- index.js
+++ index.js
@@ -1,26 +1,77 @@
-class Proxio {
+'use strict';
 
-    constructor(target, opts = {}) {
+const blacklist = [
+    'sort',
+    'reverse',
+    'splice',
+    'pop',
+    'unshift',
+    'shift',
+    'push'
+];
 
-        this.opts = Object.assign({}, opts);
+module.exports = (object, onChange) => {
+    let isBlocked = false;
 
-        return new Proxy(target, {
-            set: (obj, prop, value) => {
-                const oldValue = obj[prop];
-
-                if (oldValue !== value) {
-                    if (value && typeof value === 'object') {
-                        value = new Proxio(value, this.opts);
-                    }
-                    obj[prop] = value;
-                    if (typeof this.opts.onChange === 'function')
-                        this.opts.onChange(value, oldValue);
-                }
-
+    const handler = {
+        get(target, property, receiver) {
+            let result;
+            try {
+                result = new Proxy(target[property], handler);
+            } catch (_) {
+                console.error(_);
+                result = Reflect.get(target, property, receiver);
             }
-        });
-    }
+            return result;
+        },
+        defineProperty(target, property, descriptor) {
+            const newValue = descriptor.value;
+            const previousValue = target[property];
 
-}
+            if(newValue === previousValue) return false;
 
-module.exports = Proxio;
+            const result = Reflect.defineProperty(target, property, descriptor);
+
+            if (!isBlocked) {
+
+                const changes = {
+                    target,
+                    type: previousValue === undefined ? 'add' : 'update',
+                    property,
+                    newValue,
+                    previousValue,
+                    currentPath: ''
+                };
+
+                onChange(changes);
+            }
+
+            return result;
+        },
+        deleteProperty(target, property) {
+            const newValue = undefined;
+            const oldValue = target[property];
+
+            const result = Reflect.deleteProperty(target, property);
+
+            if (!isBlocked) {
+                onChange(newValue, oldValue);
+            }
+
+            return result;
+        },
+        apply(target, thisArg, argumentsList) {
+            if (blacklist.includes(target.name)) {
+                isBlocked = true;
+                const result = Reflect.apply(target, thisArg, argumentsList);
+                onChange();
+                isBlocked = false;
+                return result;
+            }
+
+            return Reflect.apply(target, thisArg, argumentsList);
+        }
+    };
+
+    return new Proxy(object, handler);
+};
--- test/test.js
+++ test/test.js
@@ -1,14 +1,15 @@
-const Proxio = require('../');
+const proxio = require('../');
 
 describe('proxio', function () {
     it('should be ok', function () {
         const myObj = {};
-        const myProxy = new Proxio(myObj, {
-            onChange(value, oldValue) {
-                console.log(value, oldValue)
-            }
+        const myProxy = proxio(myObj, (changes) => {
+            console.log('change', changes)
         });
         myProxy.myProp = {a: 1};
+
+        console.log(myProxy.myProp);
+
         myProxy.myProp.a = 2;
     })
 });
