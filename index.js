acf86868e5c98e22cdef5927bed8c6988e1896aa
Fabio Ricali <ricalifabio@gmail.com>
2019-01-01 22:47:13 +0100

    progress

--- index.js
+++ index.js
@@ -1,26 +1,111 @@
-class Proxio {
-
-    constructor(target, opts = {}) {
-
-        this.opts = Object.assign({}, opts);
-
-        return new Proxy(target, {
-            set: (obj, prop, value) => {
-                const oldValue = obj[prop];
-
-                if (oldValue !== value) {
-                    if (value && typeof value === 'object') {
-                        value = new Proxio(value, this.opts);
-                    }
-                    obj[prop] = value;
-                    if (typeof this.opts.onChange === 'function')
-                        this.opts.onChange(value, oldValue);
-                }
-
-            }
-        });
-    }
-
+"use strict";
+/**
+ * Simple utility function to add a new property to an existing object path.  Examples:
+ *
+ * - getPath('obj.nested', 'prop') => 'obj.nested.prop'
+ * - getPath('', 'prop') => 'prop'
+ */
+function getPath(path, prop) {
+    if (path.length !== 0)
+        return path + "." + prop;
+    else
+        return prop;
 }
 
-module.exports = Proxio;
+module.exports = (function () {
+    function _create(target, validator, manipulator, path, lastInPath) {
+        // Keeps track of the proxies we've already made so that we don't have to recreate any.
+        const proxies = {};
+
+        const proxyHandler = {
+            get(target, prop) {
+                // Special properties
+                if (prop === '__target')
+                    return target;
+                if (prop === '__isProxy')
+                    return true;
+                // Cache target[prop] for performance
+                let value = target[prop];
+                // Functions
+                if (typeof value === 'function') {
+                    return function () {
+                        const args = [];
+                        for (let _i = 0; _i < arguments.length; _i++) {
+                            args[_i] = arguments[_i];
+                        }
+                        if (validator({
+                            currentPath: path,
+                            property: lastInPath,
+                            target,
+                            type: 'function',
+                            function: prop,
+                            arguments: args
+                        })) {
+                            // If `this` is a proxy, be sure to apply to __target instead
+                            return value.apply(this.__isProxy ? this.__target : this, args);
+                        }
+                    };
+                } // Objects
+                else if (typeof value === 'object' && value !== null && target.hasOwnProperty(prop)) {
+                    // Return existing proxy if we have one, otherwise create a new one
+                    let existingProxy = proxies[prop];
+                    if (existingProxy && existingProxy.__target === value) {
+                        return existingProxy;
+                    }
+                    else {
+                        const proxy = _create(value, validator, manipulator, getPath(path, prop), prop);
+                        proxies[prop] = proxy;
+                        return proxy;
+                    }
+                }
+                // All else
+                else {
+                    return value;
+                }
+            },
+            set(target, prop, value) {
+
+                const previousValue = target[prop];
+                const currentPath = getPath(path, prop);
+
+                if (typeof manipulator === 'function') {
+                    value = manipulator(value, prop, currentPath);
+                }
+
+                if (validator({
+                    currentPath,
+                    target,
+                    type: previousValue === undefined ? 'add' : 'update',
+                    property: prop,
+                    newValue: value,
+                    previousValue
+                })) {
+                    target[prop] = value;
+                }
+                return true;
+            },
+            deleteProperty(target, prop) {
+
+                const previousValue = target[prop];
+
+                if (validator({
+                    currentPath: getPath(path, prop),
+                    target,
+                    type: 'delete',
+                    property: prop,
+                    newValue: null,
+                    previousValue
+                })) {
+                    delete target[prop];
+                }
+                return true;
+            }
+        };
+        return new Proxy(target, proxyHandler);
+    }
+    return {
+        create(target, validator, manipulator) {
+            return _create(target, validator, manipulator, '', '');
+        }
+    };
+})();
--- test/test.js
+++ test/test.js
@@ -3,12 +3,27 @@ const Proxio = require('../');
 describe('proxio', function () {
     it('should be ok', function () {
         const myObj = {};
-        const myProxy = new Proxio(myObj, {
-            onChange(value, oldValue) {
-                console.log(value, oldValue)
+        const myProxy = Proxio.create(myObj,
+            change => {
+
+                console.log(change);
+
+                if (change.property === 'b')
+                    change.newValue *= 5;
+
+                return true
+            },
+            (value, prop, path) => {
+                if (prop === 'b')
+                    return value * 5;
+                else
+                    return value;
             }
-        });
+        );
         myProxy.myProp = {a: 1};
-        myProxy.myProp.a = 2;
+        myProxy.myProp.a = {b: 5, c: [1, {d: 2}, 3]};
+        myProxy.myProp.a.b = 6;
+        myProxy.myProp.a.c[1].d = 5;
+
     })
 });
